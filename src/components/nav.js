f88080f2d7fd72af63b98d6b7a58fa1d8c0e6e72
jordanaray <47161860+jordanaray@users.noreply.github.com>
2022-02-04 22:16:32 -0700

    a tags added to nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,26 +7,26 @@ class Nav extends React.Component {
         return <nav className="nav layout vertical between-justified">
             <div>
                 <div className="layout hr-nav-bottom vertical">
-                    <img src={logo} height="20" alt="rainfocus logo"/>
-                    <span class="material-icons">search</span>
-                    <span class="material-icons">history</span>
+                    <a href="/" className="p-2"><img src={logo} height="20" alt="rainfocus logo"/></a>
+                    <a href="/"><span class="material-icons">search</span></a>
+                    <a href="/"><span class="material-icons">history</span></a>
                 </div>
 
                 <div className="layout vertical">
                     <a href="/"><span class="material-icons">home</span></a>
-                    <span class="material-icons">explore</span>
-                    <span class="material-icons">recent_actors</span>
-                    <span class="material-icons">dvr</span>
-                    <span class="material-icons">people</span>
-                    <span class="material-icons">folder_shared</span>
-                    <span class="material-icons">looks_3</span>
-                    <span class="material-icons">menu_book</span>
-                    <span class="material-icons">business</span>
-                    <span class="material-icons">equalizer</span>
+                    <a href="/"><span class="material-icons">explore</span></a>
+                    <a href="/"><span class="material-icons">recent_actors</span></a>
+                    <a href="/"><span class="material-icons">dvr</span></a>
+                    <a href="/"><span class="material-icons">people</span></a>
+                    <a href="/"><span class="material-icons">folder_shared</span></a>
+                    <a href="/"><span class="material-icons">looks_3</span></a>
+                    <a href="/"><span class="material-icons">menu_book</span></a>
+                    <a href="/"><span class="material-icons">business</span></a>
+                    <a href="/"><span class="material-icons">equalizer</span></a>
                 </div>
             </div>
             <div className="layout vertical hr-nav-top">
-                <span class="material-icons">double_arrow</span>
+                <a href="/"><span class="material-icons">double_arrow</span></a>
             </div>
 
         </nav>
